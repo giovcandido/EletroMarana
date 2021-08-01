d80156f558801ca8e0da25262cb1a7c53e47354f
giovcandido <giovcandido@outlook.com>
2021-07-31 21:46:55 -0300

    Add basic functionality to supply form

--- EletroMarana/FrmAcompanharSolicitacoes.Designer.cs
+++ EletroMarana/FrmAcompanharSolicitacoes.Designer.cs
@@ -29,22 +29,333 @@ namespace _EletroMarana
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvSolicitacoes = new System.Windows.Forms.DataGridView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cboProduto = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cboFornecedor = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtValorCusto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValorTotal = new System.Windows.Forms.TextBox();
+            this.chkChegou = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mtbDataHora = new System.Windows.Forms.MaskedTextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitacoes)).BeginInit();
             this.SuspendLayout();
             // 
-            // FrmNovaSolicitacao
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvSolicitacoes);
+            this.groupBox2.Controls.Add(this.btnPesquisar);
+            this.groupBox2.Controls.Add(this.txtPesquisa);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(13, 271);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(521, 199);
+            this.groupBox2.TabIndex = 144;
+            this.groupBox2.TabStop = false;
+            // 
+            // dgvSolicitacoes
+            // 
+            this.dgvSolicitacoes.AllowUserToAddRows = false;
+            this.dgvSolicitacoes.AllowUserToDeleteRows = false;
+            this.dgvSolicitacoes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSolicitacoes.Location = new System.Drawing.Point(9, 62);
+            this.dgvSolicitacoes.Name = "dgvSolicitacoes";
+            this.dgvSolicitacoes.ReadOnly = true;
+            this.dgvSolicitacoes.Size = new System.Drawing.Size(501, 125);
+            this.dgvSolicitacoes.TabIndex = 19;
+            this.dgvSolicitacoes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvSolicitacoes_CellClick);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(435, 17);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 18;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.BtnPesquisar_Click);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(68, 19);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(346, 20);
+            this.txtPesquisa.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Pesquisar";
+            // 
+            // cboProduto
+            // 
+            this.cboProduto.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cboProduto.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboProduto.FormattingEnabled = true;
+            this.cboProduto.Location = new System.Drawing.Point(91, 64);
+            this.cboProduto.Name = "cboProduto";
+            this.cboProduto.Size = new System.Drawing.Size(432, 21);
+            this.cboProduto.TabIndex = 146;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(10, 67);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(44, 13);
+            this.label13.TabIndex = 145;
+            this.label13.Text = "Produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 148;
+            this.label2.Text = "Código";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(91, 8);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 147;
+            this.txtID.TabStop = false;
+            // 
+            // cboFornecedor
+            // 
+            this.cboFornecedor.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cboFornecedor.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboFornecedor.FormattingEnabled = true;
+            this.cboFornecedor.Location = new System.Drawing.Point(91, 91);
+            this.cboFornecedor.Name = "cboFornecedor";
+            this.cboFornecedor.Size = new System.Drawing.Size(432, 21);
+            this.cboFornecedor.TabIndex = 150;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 149;
+            this.label1.Text = "Fornecedor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 152;
+            this.label5.Text = "Valor Custo";
+            // 
+            // txtValorCusto
+            // 
+            this.txtValorCusto.Location = new System.Drawing.Point(92, 118);
+            this.txtValorCusto.Name = "txtValorCusto";
+            this.txtValorCusto.Size = new System.Drawing.Size(130, 20);
+            this.txtValorCusto.TabIndex = 151;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 140;
+            this.label4.Text = "Quantidade";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(393, 118);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(130, 20);
+            this.txtQuantidade.TabIndex = 139;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 154;
+            this.label3.Text = "Valor Total";
+            // 
+            // txtValorTotal
+            // 
+            this.txtValorTotal.Location = new System.Drawing.Point(92, 144);
+            this.txtValorTotal.Name = "txtValorTotal";
+            this.txtValorTotal.Size = new System.Drawing.Size(130, 20);
+            this.txtValorTotal.TabIndex = 153;
+            // 
+            // chkChegou
+            // 
+            this.chkChegou.AutoSize = true;
+            this.chkChegou.Location = new System.Drawing.Point(253, 146);
+            this.chkChegou.Name = "chkChegou";
+            this.chkChegou.Size = new System.Drawing.Size(63, 17);
+            this.chkChegou.TabIndex = 155;
+            this.chkChegou.Text = "Chegou";
+            this.chkChegou.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 157;
+            this.label6.Text = "Data e Horário";
+            // 
+            // mtbDataHora
+            // 
+            this.mtbDataHora.Location = new System.Drawing.Point(91, 34);
+            this.mtbDataHora.Mask = "00/00/0000 00:00";
+            this.mtbDataHora.Name = "mtbDataHora";
+            this.mtbDataHora.ReadOnly = true;
+            this.mtbDataHora.Size = new System.Drawing.Size(126, 20);
+            this.mtbDataHora.TabIndex = 156;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(286, 209);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 160;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(205, 209);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 159;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.BtnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(367, 209);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 161;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(124, 209);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 158;
+            this.btnIncluir.Text = "Inserir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.BtnIncluir_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(448, 209);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 162;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // FrmAcompanharSolicitacoes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(546, 481);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mtbDataHora);
+            this.Controls.Add(this.chkChegou);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtValorTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.txtValorCusto);
+            this.Controls.Add(this.cboFornecedor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.cboProduto);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.groupBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.Name = "FrmNovaSolicitacao";
+            this.Name = "FrmAcompanharSolicitacoes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Acompanhar Solicitações";
+            this.Load += new System.EventHandler(this.FrmAcompanharSolicitacoes_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitacoes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvSolicitacoes;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cboProduto;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cboFornecedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtValorCusto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValorTotal;
+        private System.Windows.Forms.CheckBox chkChegou;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox mtbDataHora;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
--- EletroMarana/FrmAcompanharSolicitacoes.cs
+++ EletroMarana/FrmAcompanharSolicitacoes.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
 
 namespace _EletroMarana
 {
@@ -7,6 +10,155 @@ namespace _EletroMarana
         public FrmAcompanharSolicitacoes()
         {
             InitializeComponent();
+        }
+
+        private void FrmAcompanharSolicitacoes_Load(object sender, System.EventArgs e)
+        {
+            cboProduto.DataSource = Global.ConsultaProdutos("");
+            cboProduto.DisplayMember = "Nome";
+            cboProduto.ValueMember = "Código";
+
+            cboFornecedor.DataSource = Global.ConsultaFornecedores("");
+            cboFornecedor.DisplayMember = "Fantasia";
+            cboFornecedor.ValueMember = "Código";
+
+            LimpaCampos();
+
+            dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos("");
+        }
+
+        private void LimpaCampos()
+        {
+            txtID.Clear();
+
+            mtbDataHora.Text = DateTime.Now.ToString();
+
+            cboProduto.SelectedItem = null;
+            cboProduto.ResetText();
+
+            cboFornecedor.SelectedItem = null;
+            cboFornecedor.ResetText();
+
+            txtValorCusto.Clear();
+            txtQuantidade.Clear();
+            txtValorTotal.Clear();
+
+            chkChegou.Checked = false;
+
+            txtPesquisa.Clear();
+
+            cboProduto.Select();
+        }
+
+        private void BtnPesquisar_Click(object sender, System.EventArgs e)
+        {
+            dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos(cboProduto.Text);
+
+            txtPesquisa.Clear();
+        }
+
+        private void DgvSolicitacoes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvSolicitacoes.Rows.Count > 0)
+            {
+                txtID.Text = dgvSolicitacoes.CurrentRow.Cells[0].Value.ToString();
+                mtbDataHora.Text = dgvSolicitacoes.CurrentRow.Cells[1].Value.ToString();
+                cboProduto.Text = dgvSolicitacoes.CurrentRow.Cells[2].Value.ToString();
+                cboFornecedor.Text = dgvSolicitacoes.CurrentRow.Cells[3].Value.ToString();
+                txtValorCusto.Text = dgvSolicitacoes.CurrentRow.Cells[4].Value.ToString();
+                txtQuantidade.Text = dgvSolicitacoes.CurrentRow.Cells[5].Value.ToString();
+                txtValorTotal.Text = dgvSolicitacoes.CurrentRow.Cells[6].Value.ToString();
+                chkChegou.Checked = Convert.ToBoolean(dgvSolicitacoes.CurrentRow.Cells[7].Value.ToString());
+            }
+        }
+
+        private void BtnIncluir_Click(object sender, System.EventArgs e)
+        {
+            if (cboProduto.Text == "") return;
+
+            Global.Conexao.Open();
+
+            Global.Comando = new MySqlCommand(@"insert into abastecimento(data_hora, id_fornecedor, id_produto, valor_custo, qtd, total, chegou) 
+                                              values(?data_hora, ?id_fornecedor, ?id_produto, ?valor_custo, ?qtd, ?total, ?chegou)", Global.Conexao);
+
+            Global.Comando.Parameters.AddWithValue("?data_hora", Convert.ToDateTime(mtbDataHora.Text).ToString("yyyy-MM-dd HH:mm:ss"));
+            Global.Comando.Parameters.AddWithValue("?id_fornecedor", cboFornecedor.SelectedValue);
+            Global.Comando.Parameters.AddWithValue("?id_produto", cboProduto.SelectedValue);
+            Global.Comando.Parameters.AddWithValue("?valor_custo", txtValorCusto.Text);
+            Global.Comando.Parameters.AddWithValue("?qtd", txtQuantidade.Text);
+            Global.Comando.Parameters.AddWithValue("?total", txtValorTotal.Text);
+            Global.Comando.Parameters.AddWithValue("?chegou", Convert.ToBoolean(chkChegou.Checked));
+            Global.Comando.Parameters.AddWithValue("?id", Convert.ToInt16(txtID.Text));
+
+            Global.Comando.ExecuteNonQuery();
+
+            Global.Conexao.Close();
+
+            LimpaCampos();
+
+            dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos("");
+        }
+
+        private void BtnAtualizar_Click(object sender, System.EventArgs e)
+        {
+            if (txtID.Text == "") return;
+
+            Global.Conexao.Open();
+
+            Global.Comando = new MySqlCommand(@"update abastecimento set id_fornecedor = ?id_fornecedor, id_produto = ?id_produto, valor_custo = ?valor_custo, 
+                                              qtd = ?qtd, total = ?total, chegou = ?chegou where id = ?id", Global.Conexao);
+
+            Global.Comando.Parameters.AddWithValue("?id_fornecedor", cboFornecedor.SelectedValue);
+            Global.Comando.Parameters.AddWithValue("?id_produto", cboProduto.SelectedValue);
+            Global.Comando.Parameters.AddWithValue("?valor_custo", txtValorCusto.Text);
+            Global.Comando.Parameters.AddWithValue("?qtd", txtQuantidade.Text);
+            Global.Comando.Parameters.AddWithValue("?total", txtValorTotal.Text);
+            Global.Comando.Parameters.AddWithValue("?chegou", Convert.ToBoolean(chkChegou.Checked));
+            Global.Comando.Parameters.AddWithValue("?id", Convert.ToInt16(txtID.Text));
+
+            Global.Comando.ExecuteNonQuery();
+
+            Global.Conexao.Close();
+
+            LimpaCampos();
+
+            dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos("");
+        }
+
+        private void BtnCancelar_Click(object sender, System.EventArgs e)
+        {
+            LimpaCampos();
+
+            dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos("");
+        }
+
+        private void BtnExcluir_Click(object sender, System.EventArgs e)
+        {
+            if (txtID.Text == "") return;
+
+            if (MessageBox.Show("Deseja realmente excluir a solicitação de " + cboProduto.Text + "?", "Exclusão",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+
+                Global.Conexao.Open();
+
+                Global.Comando = new MySqlCommand("delete from abastecimentos where id = ?id", Global.Conexao);
+
+                Global.Comando.Parameters.AddWithValue("?id", Convert.ToInt16(txtID.Text));
+
+                Global.Comando.ExecuteNonQuery();
+
+                Global.Conexao.Close();
+
+                LimpaCampos();
+
+                dgvSolicitacoes.DataSource = Global.ConsultaAbastecimentos("");
+            }
+        }
+
+        private void BtnFechar_Click(object sender, System.EventArgs e)
+        {
+            Close();
         }
     }
 }
